bbcbbd623866b2f5a39d0276571a2377dd75560d
tjphillips08 <tjphillips08@icloud.com>
2022-10-19 09:53:15 -0500

    created project model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Project(models.Model):
+
+    name = models.CharField(max_length=100)
+    img = models.CharField(max_length=250)
+    github = models.CharField(max_length=250)
+    deploy = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
